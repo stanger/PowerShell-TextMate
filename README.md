c973d06f9e563d72029e24ffeb98a16ecbda656c
Troy Stanger <stanger@zumero.com>
2017-10-31 11:21:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PowerShell-TextMate
+A TextMate bundle for the PowerShell language.
+
+This repo contains a simple script for fetching the latest from [PowerShell/EditorSyntax](https://github.com/PowerShell/EditorSyntax) and building a new tmBundle.
+
+Download the bundle here: [Latest](https://github.com/stanger/PowerShell-TextMate/raw/master/dist/PowerShell.tmbundle.zip) 
+
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OUTPUT=./dist
+BUNDLE_PATH="$OUTPUT/PowerShell.tmbundle"
+
+if [ -e $BUNDLE_PATH ] ; then
+	rm -r $BUNDLE_PATH
+fi
+
+pushd TypeScript-TmLanguage
+
+git pull
+#TODO should a build be run here?
+
+popd
+
+mkdir -p $BUNDLE_PATH/Preferences
+mkdir -p $BUNDLE_PATH/Syntaxes
+
+cp EditorSyntax/*.tmLanguage $BUNDLE_PATH/Syntaxes
+cp src/* $BUNDLE_PATH
+
+pushd $OUTPUT
+
+zip -r PowerShell.tmbundle.zip PowerShell.tmbundle
+
+popd
